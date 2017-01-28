11d5363ba343a2c2cbfe86de90f3320ddcfded93
agoestchm <anselme.goetschmann@gmail.com>
2017-01-28 16:49:35 -0500

    Ignoring HDI

--- main.py
+++ main.py
@@ -7,19 +7,26 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split, cross_val_score
 
 data = pd.read_csv('fake1.csv', delimiter=';', index_col=0)
+
+# drop HDI
+data = data.drop(["HumanDevelopmentIndex(HDI)"], axis=1)
+data = data.drop(["GNIpercapitarankminusHDIrank"], axis=1)
+
 print (data.head())
 print("-----------------------------------")
+
 
 y = np.array(data["Lifeexpectancyatbirth"])
 X = np.array(data.drop(["Lifeexpectancyatbirth"], axis=1))
 X = preprocessing.scale(X)
 
-reg = LinearRegression()#svm.SVR()
+reg = LinearRegression()#
+#reg = svm.SVR()
 
 #scores = cross_val_score(reg, X, y, cv=3)
 #print(scores)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 #print(X_train)
 #print(y_train)
