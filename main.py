720128866bcca30752339dc3aaa847f521e7cbcc
agoestchm <anselme.goetschmann@gmail.com>
2017-01-28 14:48:22 -0500

    Separated train and test data

--- main.py
+++ main.py
@@ -4,12 +4,22 @@ import numpy as np
 
 from sklearn import preprocessing, svm
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 data = pd.read_csv('fake.csv', index_col=0)
 print (data)
+
 y = np.array(data["f3"])
 X = np.array(data.drop(["f3"], axis=1))
+X = preprocessing.scale(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+
+print(X_train)
+print(y_train)
 
 reg = LinearRegression()
-reg.fit (X, y)
+reg.fit (X_train, y_train)
 print(reg.coef_)
+accuracy = reg.score(X_test, y_test)
+print(accuracy)
