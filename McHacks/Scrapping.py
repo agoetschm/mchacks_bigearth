b0bda6d1428a46cd32712538db12ff5e265c649f
DenoDuz <sport_onur@hotmail.com>
2017-01-28 20:25:14 -0500

    Structured the Scrapping

--- McHacks/Scrapping.py
+++ McHacks/Scrapping.py
@@ -11,41 +11,69 @@ class Scrapping:
         readHtml = open.read()
         self.soup = BeautifulSoup(readHtml, "html.parser")
 
-    def ScrapValue(self,filename):
+    def ScrapValue(self,filename,countColCountry,countColValue):
         tableStats = self.soup.findAll("table", attrs={'class': "sortable wikitable"})
 
-        fo = open(filename + ".txt", "w")
-
+        #fo = open(filename + ".txt", "w")
+        dictValueForCountry = {}
         for myTableStats in tableStats:
-            #tbodyStats = myTableStats.find_all("tbody")
-            #for myTbodyStats in tableStats:
                 trStats = myTableStats.find_all("tr")
+
                 for myTrStats in trStats:
                     tdStats = myTrStats.find_all("td")
-                    for myTdStats in tdStats:
-                        aStats = myTdStats.find_all("a")
-                        for myAStats in aStats:
-                            val = []
-                            val.append(myAStats.get("title"))
-                            val = list(filter((None).__ne__, val))
-                            for v in val:
-                                fo.write(v)
-                                #print(v)
+                    countColNumber=0
+                    vCountry = None
+                    vValue= None
+                    vValidLine =True
 
-                        myTdStats = str(myTdStats)
-                        start = myTdStats.index(">") + 1
-                        end = myTdStats[start:].index("<")
-                        if myTdStats[start:(start + end)] != '0':
-                            #print(myTdStats[start:(start + end)])
-                            fo.write(myTdStats[start:(start + end)] + "\n");
+                    for myTdStats in tdStats:
+
+                        if vValidLine:
+                            if countColNumber == countColCountry:
+                                aStats = myTdStats.find_all("a")
+                                if aStats:
+
+                                    for myAStats in aStats:
+                                        country = myAStats.get("title") # This shows only the countries with titles
+                                        if country:
+                                            # fo.write(v)
+                                            #print("true")
+                                            print(country)
+                                            vCountry=country
+                                        else:
+                                            #print("false")
+                                            vValidLine=False
+                                else:
+                                    #print("false")
+                                    vValidLine=False
+                            elif(countColNumber==countColValue):
+
+                                myTdStats = str(myTdStats)
+                                start = myTdStats.index(">") + 1
+                                end = myTdStats[start:].index("<")
+                                tdValue = myTdStats[start:(start + end)]
+                                if (tdValue):
+                                    #print(tdValue)
+                                    vValue=tdValue
+                                else:
+                                    #print("None")
+                                    vValue=None
+
                         else:
-                            continue
-        fo.close()
+                            break
+                        countColNumber += 1
+                    if vValidLine :
+                        dictValueForCountry[vCountry]=vValue
+
+
+        #fo.close()
+        print(dictValueForCountry)
+        return dictValueForCountry
 
     def BirthRate(self):
         url = "https://en.wikipedia.org/wiki/List_of_sovereign_states_and_dependent_territories_by_birth_rate"
         self.OpenURL(url)
-        self.ScrapValue("BirthRate")
+        self.ScrapValue("BirthRate",0,7)
 
     def DeathRate(self):
         url = "https://en.wikipedia.org/wiki/List_of_sovereign_states_and_dependent_territories_by_mortality_rate"
