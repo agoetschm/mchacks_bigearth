5c231d5692d9d8acb49adb1fbdaaacfd1f78123c
DenoDuz <sport_onur@hotmail.com>
2017-01-28 23:30:25 -0500

    Scrapping

--- McHacks/Main.py
+++ McHacks/Main.py
@@ -4,10 +4,7 @@ from TextFileReader import TextFileReader
 
 class Main():
     if __name__=="__main__":
+
         scrappingObj = Scrapping()
-        textfilereaderObj = TextFileReader()
 
-        scrappingObj.BirthRate()
-        scrappingObj.DeathRate()
-
-        textfilereaderObj.Reader()
+        scrappingObj.getScrappedData()
--- McHacks/Scrapping.py
+++ McHacks/Scrapping.py
@@ -2,82 +2,120 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 class Scrapping:
-    def __init__(self):
-        self.soup =None
 
     def OpenURL(self,url):
         req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
         open = urllib.request.urlopen(req)
         readHtml = open.read()
-        self.soup = BeautifulSoup(readHtml, "html.parser")
+        return BeautifulSoup(readHtml, "html.parser")
 
-    def ScrapValue(self,filename,countColCountry,countColValue):
-        tableStats = self.soup.findAll("table", attrs={'class': "sortable wikitable"})
+    def getScrappedFeature(self,url,index,countColCountry,countColValue,year):
+        print(url)
+
+        soup = self.OpenURL(url)
+        tableStats = soup.findAll("table", attrs={'class': "wikitable"})
 
         #fo = open(filename + ".txt", "w")
         dictValueForCountry = {}
-        for myTableStats in tableStats:
-                trStats = myTableStats.find_all("tr")
+        myTableStats = tableStats[index]
+        #for myTableStats in tableStats:
+        trStats = myTableStats.find_all("tr")
 
-                for myTrStats in trStats:
-                    tdStats = myTrStats.find_all("td")
-                    countColNumber=0
-                    vCountry = None
-                    vValue= None
-                    vValidLine =True
+        for myTrStats in trStats:
+            tdStats = myTrStats.find_all("td")
+            countColNumber=0
+            vCountry = None
+            vValue= None
+            hasLink =False
+            hasCountry =False
 
-                    for myTdStats in tdStats:
+            for myTdStats in tdStats:
 
-                        if vValidLine:
-                            if countColNumber == countColCountry:
-                                aStats = myTdStats.find_all("a")
-                                if aStats:
-
-                                    for myAStats in aStats:
-                                        country = myAStats.get("title") # This shows only the countries with titles
-                                        if country:
-                                            # fo.write(v)
-                                            #print("true")
-                                            print(country)
-                                            vCountry=country
-                                        else:
-                                            #print("false")
-                                            vValidLine=False
-                                else:
-                                    #print("false")
-                                    vValidLine=False
-                            elif(countColNumber==countColValue):
-
-                                myTdStats = str(myTdStats)
-                                start = myTdStats.index(">") + 1
-                                end = myTdStats[start:].index("<")
-                                tdValue = myTdStats[start:(start + end)]
-                                if (tdValue):
-                                    #print(tdValue)
-                                    vValue=tdValue
-                                else:
-                                    #print("None")
-                                    vValue=None
-
+                if not hasLink or hasCountry:
+                    if countColNumber == countColCountry:
+                        aStats = myTdStats.find_all("a")
+                        if aStats:
+                            hasLink = True
+                            #print(aStats)
+                            myAStats = aStats[0]
+                            #for myAStats in aStats:
+                            country = myAStats.get("title") # This shows only the countries with titles
+                            if country:
+                                # fo.write(v)
+                                #print("true")
+                                #print(country)
+                                vCountry=country
+                                hasCountry = True
+                            else:
+                                pass
+                                #print("false")
                         else:
-                            break
-                        countColNumber += 1
-                    if vValidLine :
-                        dictValueForCountry[vCountry]=vValue
+                            pass
+                            #print("false")
+                    elif(countColNumber==countColValue):
+
+                        myTdStats = str(myTdStats)
+                        start = myTdStats.index(">") + 1
+                        end = myTdStats[start:].index("<")
+                        tdValue = myTdStats[start:(start + end)]
+                        if (tdValue):
+                            #print(tdValue)
+                            vValue=tdValue
+                        else:
+                            #print("None")
+                            vValue=None
+
+
+
+
+                else:
+                    break
+                countColNumber += 1
+            if hasCountry :
+                #print("HEYYYYYYYYYYYYY"+str(vCountry))
+                dictValueForCountry[str(vCountry)+"/"+year]=vValue
+
 
 
         #fo.close()
-        print(dictValueForCountry)
+        #print(dictValueForCountry)
         return dictValueForCountry
 
-    def BirthRate(self):
-        url = "https://en.wikipedia.org/wiki/List_of_sovereign_states_and_dependent_territories_by_birth_rate"
-        self.OpenURL(url)
-        self.ScrapValue("BirthRate",0,7)
 
-    def DeathRate(self):
-        url = "https://en.wikipedia.org/wiki/List_of_sovereign_states_and_dependent_territories_by_mortality_rate"
-        self.OpenURL(url)
-        self.ScrapValue("DeathRate")
+
+    def getScrappedData(self):
+        params = [["BirthRate","2014","https://en.wikipedia.org/wiki/List_of_sovereign_states_and_dependent_territories_by_birth_rate",0, 0, 7],
+                  ["DeathRate","2014","https://en.wikipedia.org/wiki/List_of_sovereign_states_and_dependent_territories_by_mortality_rate",0, 0,3],
+                  ["FertilityRate","2014","https://en.wikipedia.org/wiki/List_of_sovereign_states_and_dependent_territories_by_fertility_rate",0, 1,2],
+                  ["HappinessRate","2016","https://en.wikipedia.org/wiki/World_Happiness_Report",0, 1,2],
+                  ["HDI","2014","https://en.wikipedia.org/wiki/List_of_countries_by_Human_Development_Index",0, 2,3],
+                  ["HDI","2014","https://en.wikipedia.org/wiki/List_of_countries_by_Human_Development_Index",1, 2,3],
+                  ["HDI","2014","https://en.wikipedia.org/wiki/List_of_countries_by_Human_Development_Index",2, 2,3],
+                  ["HDI","2014","https://en.wikipedia.org/wiki/List_of_countries_by_Human_Development_Index",3, 2,3],
+                  ["HDI","2014","https://en.wikipedia.org/wiki/List_of_countries_by_Human_Development_Index",4, 2,3],
+                  ["HDI","2014","https://en.wikipedia.org/wiki/List_of_countries_by_Human_Development_Index",5, 2,3],
+                  ["HDI","2014","https://en.wikipedia.org/wiki/List_of_countries_by_Human_Development_Index",6, 2,3],
+                  ["HDI","2014","https://en.wikipedia.org/wiki/List_of_countries_by_Human_Development_Index",7, 2,3]
+
+
+                  ]
+
+        scrappedData = {}
+        scrappedData["labels"] = []
+
+        for label,year,url,index, countryCol, valueCol in params:
+            scrappedData["labels"].append(label)
+            newFeatureDict = self.getScrappedFeature(url,index,countryCol,valueCol,year)
+            for country, feature in newFeatureDict.items():
+                if country in scrappedData:
+                    scrappedData[country].append(feature)
+                else:
+                    scrappedData[country] = (len(scrappedData["labels"])-1) * [None] + [feature]
+
+
+
+        print(scrappedData)
+        return scrappedData
+
 
 
